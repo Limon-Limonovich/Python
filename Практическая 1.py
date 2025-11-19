952797ca90b819efefd4d3ca04ca990867ad9b30
Limon-Limonovich <limonchic325523@gmail.com>
2025-11-19 10:41:39 +0300

    Практическая 1.py

---
+++ Практическая 1.py
@@ -0,0 +1,26 @@
+# Задание 1
+m = ['круг', 0.25, 'квадрат', 'треугольник', 15, 'круг', 'овал', '10']
+m.remove(0.25)
+m.remove(15)
+m.remove('10')
+print(m)
+
+#Задание 2
+abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+del abc[1:5]
+print(abc)
+
+# Задание 3
+numbers = [1, 4, 2, 6, 3, 5]
+numbers.sort()
+print(numbers)
+# Задание 4
+mass = [14, -6, 3, 11, 6, 8.5, 99, -20, -6, 10, 40, 0.25, -4, 5]
+mass.sort()
+print(mass)
+del mass [0:4]
+print(mass)
+mass.sort(reverse = True)
+print(mass)
+
+# Задание 5
